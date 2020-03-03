20fd07d63608338622a3a4eaaa38999fa5a2ced8
Roy Kuijper <roykuijper199716@hotmail.com>
2020-03-03 14:44:19 +0100

    fixed some merge conflicts

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -1,30 +1,33 @@
 // Render
 export default function renderBooks(data) {
-    const container = document.querySelector('.overview');
-    let results = data;
+  const container = document.querySelector(".overview");
+  let results = data;
 
-    console.log('data', data);
+  console.log("data", data);
 
-    document.querySelector('.wrapper').classList.toggle('hide');
-    document.querySelector('.suggestion-wrapper').classList.toggle('hide');
-    document.querySelector('.overview').classList.toggle('show');
+  document.querySelector(".wrapper").classList.toggle("hide");
+  document.querySelector(".content-wrapper").classList.toggle("hide");
+  document.querySelector(".overview").classList.toggle("show");
 
-    if (results.length !== 0) {
-        // Render new data
-        results.forEach((item) => {
-            const html = `
+  if (results.length !== 0) {
+    // Render new data
+    results.forEach(item => {
+      const html = `
                     <article>
                     <h2>${item.titles[0]}</h2>
-                    <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
+                    <p>${
+                      item.summaries ? item.summaries[0] : "Geen samenvatting"
+                    }</p>
                     <img src="${
-                        item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
+                      item.coverimages
+                        ? item.coverimages[1]
+                        : "Geen samenvatting"
                     }">
                     </article>
                 `;
-            container.insertAdjacentHTML('beforeend', html);
-        });
-    }
-    else {
-        container.insertAdjacentHTML('beforeend', 'Geen boeken gevonden!');
-    }
+      container.insertAdjacentHTML("beforeend", html);
+    });
+  } else {
+    container.insertAdjacentHTML("beforeend", "Geen boeken gevonden!");
+  }
 }
