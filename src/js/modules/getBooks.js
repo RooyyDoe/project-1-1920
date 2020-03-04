a842757f5ed375d49969260e9453cd7070ea3b81
Roy Kuijper <roykuijper199716@hotmail.com>
2020-03-04 15:42:58 +0100

    before pull

--- src/js/modules/getBooks.js
+++ src/js/modules/getBooks.js
@@ -2,19 +2,21 @@ import { Fetcher } from "./fetcher.js";
 
 // Get books
 export default function getBooks(query) {
-    // set endpoint url values
-    const cors = "https://cors-anywhere.herokuapp.com/";
-    const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
-    const key = "cdb8415c172ec6178b63451e222891a6";
-    const secret = "31978bcbb4e5eb7239f01c180e2f07b1";
-    const detail = "Default";
-    const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+  // set endpoint url values
+  const cors = "https://cors-anywhere.herokuapp.com/";
+  const endpoint =
+    "https://zoeken.oba.nl/api/v1/search/?id=|oba-catalogus|1019761";
+  const key = "cdb8415c172ec6178b63451e222891a6";
+  const secret = "31978bcbb4e5eb7239f01c180e2f07b1";
+  const detail = "Default";
+  const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
 
-    const config = {
-      Authorization: `Bearer ${secret}`
-    };
+  const config = {
+    Authorization: `Bearer ${secret}`
+  };
 
-    // Fetch
-    return Fetcher.get(url, config)
-        .then(data => {return data.results})
+  // Fetch
+  return Fetcher.get(url, config).then(data => {
+    return data.results;
+  });
 }
