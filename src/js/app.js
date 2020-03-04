aa42d7015e10113f2cb9ffe0f4b171ad270ec6c2
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-04 10:26:27 +0100

    add favourites

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,18 @@ import search from './modules/utils.js'
 import './vendor/routie.js'
 
 routie({
-  // Search field + genres
-  '': () => {
-    document.querySelector("#search").addEventListener("click", search);
-  }
-  // Books overview
+    // Search field + genres
+    '': () => {
+        document.querySelector("#search").addEventListener("click", search);
+    },
+    ':overview': () => {  
+        console.log('overview');},
+    // Books favourite
+    '/:favorieten': () => {
+        console.log('favorieten');
+        document.querySelector(".wrapper").classList.toggle("hide");
+        document.querySelector(".content-wrapper").classList.toggle("hide");
+        document.querySelector(".my-favourites").classList.toggle("hide");
+        document.querySelector(".favourites").classList.toggle("show");
+    }
 });
