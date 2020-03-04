b81e02d1175e44e7b0ebe1c2acbfc9366442a264
Roy Kuijper <roykuijper199716@hotmail.com>
2020-03-04 10:25:56 +0100

    added template engine

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -8,26 +8,16 @@ export default function renderBooks(data) {
   document.querySelector(".wrapper").classList.toggle("hide");
   document.querySelector(".content-wrapper").classList.toggle("hide");
   document.querySelector(".overview").classList.toggle("show");
-  document.querySelector(".overview-header").classList.toggle("show");
 
   if (results.length !== 0) {
-    // Render new data
-    results.forEach(item => {
-      const html = `
-                    <article>
-                    <h2>${item.titles[0]}</h2>
-                    <p>${
-                      item.summaries ? item.summaries[0] : "Geen samenvatting"
-                    }</p>
-                    <img src="${
-                      item.coverimages
-                        ? item.coverimages[1]
-                        : "Geen samenvatting"
-                    }">
-                    </article>
-                `;
-      container.insertAdjacentHTML("beforeend", html);
-    });
+    const overviewTemplate = document.getElementById("overviewTemplate")
+      .textContent;
+    const compiledTemplate = Handlebars.compile(overviewTemplate);
+    const bookOverviewGeneratedHTML = compiledTemplate(data);
+
+    const container = document.querySelector(".overview");
+    console.log(bookOverviewGeneratedHTML);
+    container.insertAdjacentHTML("afterbegin", bookOverviewGeneratedHTML);
   } else {
     container.insertAdjacentHTML("beforeend", "Geen boeken gevonden!");
   }
