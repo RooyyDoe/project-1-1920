9b32b81f4d71af5d8bf50cd72703adec0dc99a46
Roy Kuijper <roykuijper199716@hotmail.com>
2020-03-05 17:26:53 +0100

    fixed the last bugs

--- src/js/modules/template.js
+++ src/js/modules/template.js
@@ -1,26 +1,44 @@
 document.querySelectorAll(".radio-buttons-background label").forEach(label => {
-    label.addEventListener("click", () => {
-        const color = label.getAttribute('for');
-        document.querySelector('.template').setAttribute('style', `background-color: ${color}`)
-    });
+  label.addEventListener("click", () => {
+    const color = label.getAttribute("for");
+    document
+      .querySelector(".template")
+      .setAttribute("style", `background-color: ${color}`);
+  });
 });
 
 document.querySelectorAll(".radio-buttons-font label").forEach(label => {
-    label.addEventListener("click", () => {
-        const font = label.getAttribute('for');
-        document.querySelector('#section1 > div').setAttribute('style', `font-family: ${font}`);
-        document.querySelector('#section2 > div').setAttribute('style', `font-family: ${font}`);
-        document.querySelector('#section3 > div').setAttribute('style', `font-family: ${font}`);
-        document.querySelector('#section4 > div').setAttribute('style', `font-family: ${font}`);
-    });
+  label.addEventListener("click", () => {
+    const font = label.getAttribute("for");
+    document
+      .querySelector("#section1 > div")
+      .setAttribute("style", `font-family: ${font}`);
+    document
+      .querySelector("#section2 div")
+      .setAttribute("style", `font-family: ${font}`);
+    document
+      .querySelector("#section3 div")
+      .setAttribute("style", `font-family: ${font}`);
+    document
+      .querySelector("#section4 div")
+      .setAttribute("style", `font-family: ${font}`);
+  });
 });
 
 document.querySelectorAll(".radio-buttons-text-color label").forEach(label => {
-    label.addEventListener("click", () => {
-        const color = label.getAttribute('for');
-        document.querySelector('#section1').setAttribute('style', `color: ${color}`)
-        document.querySelector('#section2').setAttribute('style', `color: ${color}`)
-        document.querySelector('#section3').setAttribute('style', `color: ${color}`)
-        document.querySelector('#section4').setAttribute('style', `color: ${color}`)
-    });
+  label.addEventListener("click", () => {
+    const color = label.getAttribute("for");
+    document
+      .querySelector("#section1")
+      .setAttribute("style", `color: ${color}`);
+    document
+      .querySelector("#section2")
+      .setAttribute("style", `color: ${color}`);
+    document
+      .querySelector("#section3")
+      .setAttribute("style", `color: ${color}`);
+    document
+      .querySelector("#section4")
+      .setAttribute("style", `color: ${color}`);
+  });
 });
